cb3812e14ac3e5aae13beca4732495a7a3ad8f6c
Lam4ek <d.i.iljyusha@student.khai.edu>
2022-12-26 15:40:59 +0100

    ModalWindow 1-st stage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from 'react';
 import './index.scss';
 
 function App() {
-	const [count, setCount] = React.useState(0);
-	const onClickPlus = () => {
-		setCount (count + 1)
-	}
-	const onClickMinus = () => {
-		setCount (count - 1)
-	}
-  return (
-    <div className="App">
-      <div>
-        <h2>Counter:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">- Minus</button>
-				<button onClick={onClickPlus} className="plus">Plus +</button>
-      </div>
-    </div>
-  );
+	const[open, setOpen] = React.useState(false)
+	return (
+		<div className="App">
+			<button onClick={()=> setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+			{
+				open && <div className="overlay">
+					<div className="modal">
+						<svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+							<title />
+							<path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+						</svg>
+						<img src="https://media.giphy.com/media/h408T6Y5GfmXBKW62l/giphy.gif" />
+					</div>
+				</div>
+			}
+		</div>
+	);
 }
 
 export default App;
