6d799207332b5ed80769c67f7387c2d5fb312694
Lam4ek <d.i.iljyusha@student.khai.edu>
2022-12-26 16:38:58 +0100

    Quiz 1-st stage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
-import React from 'react';
+import React from 'react'
 import './index.scss';
 
-function App() {
-	const[open, setOpen] = React.useState(false)
+const questions = [
+	{
+		title: 'React - is ... ?',
+		variants: ['library', 'framework', 'application'],
+		correct: 0,
+	},
+	{
+		title: 'The component is ... ',
+		variants: ['application', 'part of an application or page', "what I don't know what is"],
+		correct: 1,
+	},
+	{
+		title: 'What is JSX?',
+		variants: [
+			'This is plain HTML',
+			'This is a function',
+			'This is the same HTML, but with the ability to execute JS code',
+		],
+		correct: 2,
+	},
+];
+
+function Result({correct}) {
 	return (
-		<div className="App">
-			<button onClick={()=> setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+		<div className="result">
+			<img alt='icon' src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>
+			<h2>You guessed {correct} answers out of {questions.length}</h2>
+			<a href='./'>
+				<button>Try again</button>
+			</a>
+		</div>
+	);
+}
+
+function Game({ step, question, onClickVariant }) {
+	const persent = Math.round((step / questions.length) * 100);
+	return (
+		<>
+			<div className="progress">
+				<div style={{ width: `${persent}%` }} className="progress__inner"></div>
+			</div>
+			<h1>{question.title}</h1>
+			<ul>
+				{
+					question.variants.map((text, index) => <li onClick={() => onClickVariant(index)} key={text}>{text}</li>)
+				}
+			</ul>
+		</>
+	);
+}
+
+function App() {
+	const[step, setStep] = React.useState(0);
+	const [correct, setCorrect] = React.useState(0);
+	const question = questions[step];
+	const onClickVariant = (index) =>{
+		console.log(step, index);
+		setStep(step + 1)
+		if(index === question.correct){
+			setCorrect(correct + 1);
+		}
+	}
+	return (
+		<div className="app">
 			{
-				open && <div className="overlay">
-					<div className="modal">
-						<svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-							<title />
-							<path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-						</svg>
-						<img src="https://media.giphy.com/media/h408T6Y5GfmXBKW62l/giphy.gif" />
-					</div>
-				</div>
+				step !== questions.length ? <Game step={step} question={question} onClickVariant={onClickVariant} /> : <Result correct={correct} />
 			}
 		</div>
 	);
