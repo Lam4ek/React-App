feffd05a3b4ca308b3bff050a0fcb2cea17d6478
Lam4ek <d.i.iljyusha@student.khai.edu>
2023-01-05 15:11:13 +0100

    modalWindow

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './index.scss';
 function App() {
 	const[open, setOpen] = React.useState(false)
 	return (
-		<div className="App">
+		<div className="app">
 			<button onClick={()=> setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
 			{
 				open && <div className="overlay">
@@ -13,7 +13,7 @@ function App() {
 							<title />
 							<path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
 						</svg>
-						<img src="https://media.giphy.com/media/h408T6Y5GfmXBKW62l/giphy.gif" />
+						<img alt='gif' src="https://media.giphy.com/media/h408T6Y5GfmXBKW62l/giphy.gif" />
 					</div>
 				</div>
 			}
